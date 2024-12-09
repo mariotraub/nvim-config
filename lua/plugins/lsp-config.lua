51eda0780e8dc68cfd95a8bcc7eae3090f5e025f
mtraub <mario.traub@ergon.ch>
2024-12-09 10:04:21 +0100

    img-clip

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,6 +26,9 @@ return {
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})

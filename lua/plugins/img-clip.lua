09cb14258521c237f86be053c7444dc4f185f824
mtraub <mario.traub@ergon.ch>
2024-12-10 10:02:41 +0100

    use <leader>c to copy to windows clipboard in visual mode

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -1,27 +1,9 @@
 return {
 	"HakonHarnes/img-clip.nvim",
 	event = "VeryLazy",
-	opts = {},
-	config = function()
-        require("img-clip").setup({
-            img_dir = function()
-                -- Absoluten Pfad zur aktuellen Datei holen
-                local current_file = vim.fn.expand("%:p")
-                -- Verzeichnis der Datei extrahieren
-                local current_dir = vim.fn.fnamemodify(current_file, ":h")
-                -- Relativen Ordner "img" erstellen
-                local img_folder = current_dir .. "/img"
-                -- Verzeichnis sicherstellen
-                vim.fn.mkdir(img_folder, "p")
-                return img_folder
-            end,
-            img_name = function()
-                -- Name des Bildes mit Zeitstempel
-                return os.date("%Y-%m-%d_%H-%M-%S")
-            end,
-        })
-    end,
+	opts = {
+	},
 	keys = {
 		{ "<leader>img", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-	},
+	}
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,3 +2,5 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.cmd("set mouse=")
+
+vim.keymap.set('v', '<leader>c', ":'<,'>w !perl -pe 'chomp if eof' | clip.exe<CR>", {})

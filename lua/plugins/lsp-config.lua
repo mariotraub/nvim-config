2a2a0064de31ef85f27159cf02518bad4f3f9cf4
mtraub <mario.traub@ergon.ch>
2024-12-19 10:22:33 +0100

    auto config lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,11 +16,7 @@ return {
 
 			mason_lspconfig.setup_handlers {
 				function (ls)
-					local config = require('pynappo/plugins/lsp/config/defaults')
-					local ok, override = pcall(require, 'pynappo/plugins/lsp/config/' .. ls)
-					if ok then
-						config = vim.tbl_deep_extend('force', config, override)
-					end
+					local config = require("cmp_nvim_lsp").default_capabilities()
 					require('lspconfig')[ls].setup(config)
 				end
 			}
@@ -29,28 +25,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			local lspconfig = require("lspconfig")
-
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.yamlls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-
-
-
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})

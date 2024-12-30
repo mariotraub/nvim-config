ba46f2040eb484d85eb126f8a6e788bf11f35703
mtraub <mario.traub@ergon.ch>
2024-12-30 20:28:26 +0100

    display absolute linenumber instead of 0 for current line

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,6 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.cmd("set mouse=")
 
-vim.cmd("set relativenumber")
+vim.cmd("set number relativenumber")
 
 vim.keymap.set('v', '<leader>c', "y:call system('perl -pe \"chomp if eof\" | clip.exe', @\")<CR>", {})

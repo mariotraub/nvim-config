2e22dccf25775228c99aa4c8acbefcbf2ad27464
mtraub <mario.traub@ergon.ch>
2024-12-31 15:40:59 +0100

    add fugitive and set add all and commit shortcut to <leader>gc

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,17 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function ()
-		require("gitsigns").setup()
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>")
-		vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
-	end
+			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk_inline<CR>")
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>")
+		end
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gc", ":Git add . | Git commit<CR>")
+		end
+	}
 }

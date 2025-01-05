15c5f5ac4274ea938ac6773aba04859bfc284ad6
mtraub <mario.traub@ergon.ch>
2025-01-05 15:19:48 +0100

    add completion for relative files

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -7,6 +7,7 @@ return {
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-path"
 		},
 	},
 	{
@@ -37,8 +38,9 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 				}, {
-					{ name = "buffer" },
-				}),
+						{ name = "path" },
+						{ name = "buffer" },
+					}),
 			})
 		end,
 	},

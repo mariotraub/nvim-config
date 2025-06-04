f689b04ce65caf966c4d4b18f43b90fc44c0335b
Marco Matteo <marco.matteo@edu.tbz.ch>
2025-06-04 11:28:21 +0200

    neo-tree show hidden files + connect local clipboard

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,14 @@ return {
 		"MunifTanjim/nui.nvim"
 	},
 	config = function()
-		require("neo-tree").setup({	
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = flase,
+				}
+			},
 			event_handlers = {
 				{
 					event = "file_open_requested",

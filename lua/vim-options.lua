d64b795f3057a7eed09f1a00772b04eedb51f6fb
Marco Matteo <marco.matteo@edu.tbz.ch>
2026-04-22 10:22:35 +0200

    ignore lock file + enable arrow keys (it's not that serious)

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -25,10 +25,3 @@ vim.opt.colorcolumn = "120"
 vim.keymap.set("n", "<leader>br", ":%s/\\r//g<CR>", {})
 
 local modes = {"n", "i", "v"}
-
-for _, mode in ipairs(modes) do
-	vim.keymap.set(mode, "<Up>", function() vim.cmd("echo 'Arrow ↑ disabled'") end, { silent = true })
-	vim.keymap.set(mode, "<Down>", function() vim.cmd("echo 'Arrow ↓ disabled'") end, { silent = true })
-	vim.keymap.set(mode, "<Left>", function() vim.cmd("echo 'Arrow ← disabled'") end, { silent = true })
-	vim.keymap.set(mode, "<Right>", function() vim.cmd("echo 'Arrow → disabled'") end, { silent = true })
-end
